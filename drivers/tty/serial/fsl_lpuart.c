55984a4395eb8492fd97d714024267ce50cb0062
Fugang Duan <fugang.duan@nxp.com>
2019-01-28 10:27:27 +0800

    tty: serial: fsl_lpuart: add magic SysRq support
    Add magic SysRq key support.
    
    Signed-off-by: Fugang Duan <fugang.duan@nxp.com>

--- drivers/tty/serial/fsl_lpuart.c
+++ drivers/tty/serial/fsl_lpuart.c
@@ -881,7 +881,12 @@ static irqreturn_t lpuart32_rxint(int irq, void *dev_id)
 		 */
 		sr = lpuart32_read(&sport->port, UARTSTAT);
 		rx = lpuart32_read(&sport->port, UARTDATA);
-		rx &= 0x3ff;
+
+		if ((sr & UARTSTAT_FE) && (rx & UARTDATA_FRETSC) &&
+			!(rx & UARTDATA_MASK)) {
+			if (uart_handle_break(&sport->port))
+				continue;
+		}
 
 		if (uart_handle_sysrq_char(&sport->port, (unsigned char)rx))
 			continue;
